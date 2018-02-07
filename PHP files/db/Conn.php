182acd45ce0ab87bf4629e81696215a15774300b
Bobby Jasuja <bobbyjasuja@gmail.com>
2018-02-07 16:05:20 -0500

    Delete Conn.php

--- PHP files/db/Conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Conn {
-    public static $dbhost = "localhost";
-    public static $dbuser = "iosgroup_bjprog";
-    public static $dbpassword = "iosgroup15syr";
-    Public static $dbname = "iosgroup_id1630591";
-}
-?>
