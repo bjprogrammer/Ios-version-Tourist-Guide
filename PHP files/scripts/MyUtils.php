927340d171c95ef228c53148ddc5cc072591ffaf
Bobby Jasuja <bobbyjasuja@gmail.com>
2018-02-07 16:00:10 -0500

    Delete MyUtils.php

--- PHP files/scripts/MyUtils.php
+++
@@ -1,9 +0,0 @@
-<?php
-class MyUtils {
- 
-    static function generateTokenString($length = 10) {
-      return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
-    }
-}
-
-?>
